5134d4b35f89e3d8d22108dea5a44d177c2fee86
Lothar Rubusch <l.rubusch@gmx.ch>
2019-11-07 22:54:35 +0100

    build.sh: update with correct defconfig file

--- docker__buildroot/build.sh
+++ docker__buildroot/build.sh
@@ -1,13 +1,8 @@
 #!/bin/bash -e
-##
-## started with:
-## $ docker run -ti -v $PWD/mnt:$(pwd)/output afb489d932bc
-##
-#export DEFCONFIG=raspberrypi3__lothar_defconfig
-export DEFCONFIG=de1soc__lothar_defconfig
 
 ## avoid checks for being "root"
 export FORCE_UNSAFE_CONFIGURE=1
+export DEFCONFIG=lothars__socrates_cyclone5_defconfig
 
 cd buildroot
 make ${DEFCONFIG}
