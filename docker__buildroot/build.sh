a23ab1111ae18df2b271f94f8e0ededf96b2ff4f
Lothar Rubusch <l.rubusch@gmx.ch>
2019-11-08 16:45:56 +0100

    Dockerfile: fix paths for unprivileged user

--- docker__buildroot/build.sh
+++ docker__buildroot/build.sh
@@ -1,12 +1,9 @@
 #!/bin/bash -e
-
-## avoid checks for being "root"
-export FORCE_UNSAFE_CONFIGURE=1
 export DEFCONFIG=lothars__socrates_cyclone5_defconfig
 
-cd buildroot
+cd ~/buildroot
 make ${DEFCONFIG}
 make -j8
 
 ## obtain build artifacts
-cp -arvf /root/buildroot/output/* /mnt/
+cp -arvf ~/buildroot/output/* /mnt/
