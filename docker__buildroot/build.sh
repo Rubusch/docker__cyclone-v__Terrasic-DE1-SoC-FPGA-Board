4f7f1f9150f3b2c5b85195655488db4c8a84de5c
Lothar Rubusch <l.rubusch@gmx.ch>
2019-11-16 15:49:07 +0100

    build.sh: no copy and chown needed for mount with file_move patch

--- docker__buildroot/build.sh
+++ docker__buildroot/build.sh
@@ -1,15 +1,5 @@
 #!/bin/bash -e
 export DEFCONFIG=lothars__socrates_cyclone5_defconfig
-
 cd ~/buildroot
 make ${DEFCONFIG}
 make -j8
-## not possible to mount the folder directly as 'output' inside the container
-## buildroot's make then fails right in the beginning with:
-##
-##   *** Error during update of the configuration.
-##
-
-## obtain build artifacts
-sudo chown $(whoami):$(whoami) -R ~/output
-cp -arvfL ~/buildroot/output/* ~/output/
