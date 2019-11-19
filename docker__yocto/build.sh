5d15871cc86fbed6e48f68fd5eaf66c2f366116a
Lothar Rubusch <l.rubusch@gmx.ch>
2019-11-19 10:51:37 +0100

    build.sh: update

--- docker__yocto/build.sh
+++ docker__yocto/build.sh
@@ -4,46 +4,49 @@
 export BUILDDIR=~/poky/build
 chown $(whoami):$(whoami) -R $BUILDDIR
 
-## is this a rebuild? if not, init the confis
-if [[ ! -f $BUILDDIR/conf/bblayers.conf ]]; then
-    cd ~/poky
-    source oe-init-build-env $BUILDDIR
+## backup and wipe out configs (automatic build
+test -f $BUILDDIR/conf/bblayers.conf && cp -arf $BUILDDIR/conf/bblayers.conf{,.orig}
+test -f $BUILDDIR/conf/local.conf && cp -arf $BUILDDIR/conf/local.conf{,.orig}
+rm -f $BUILDDIR/conf/bblayers.conf
+rm -f $BUILDDIR/conf/local.conf
 
-    ## adjust bblayers.conf
-    sed -i '/  "/s/.*/  \/home\/user\/poky\/meta-altera \\/g' $BUILDDIR/conf/bblayers.conf
-    ## using meta-openembedded - why?
-    #sed -i '/  "/s/.*/  \/home\/user\/poky\/meta-openembedded \\/g' $BUILDDIR/conf/bblayers.conf
-    #echo '  /home/user/poky/meta-altera \' >> $BUILDDIR/conf/bblayers.conf
-    echo '  "' >> $BUILDDIR/conf/bblayers.conf
-fi
+cd ~/poky
+source oe-init-build-env $BUILDDIR
 
-if [[ ! -f $BUILDDIR/conf/local.conf ]]; then
-    ## adjustments to local.conf
+## adjust bblayers.conf
+sed -i '/  "/s/.*/  \/home\/user\/poky\/meta-altera \\/g' $BUILDDIR/conf/bblayers.conf
+## using meta-openembedded - why?
+#sed -i '/  "/s/.*/  \/home\/user\/poky\/meta-openembedded \\/g' $BUILDDIR/conf/bblayers.conf
+#echo '  /home/user/poky/meta-altera \' >> $BUILDDIR/conf/bblayers.conf
+echo '  "' >> $BUILDDIR/conf/bblayers.conf
 
-    ## MACHINE
-    sed -i '/MACHINE ??= "qemux86"/s/.*/MACHINE ?= "cyclone5"/g' $BUILDDIR/conf/local.conf
 
-    sed -i '/PACKAGE_CLASSES ?= "package_rpm"/s/.*/PACKAGE_CLASSES ?= "package_deb"/g' $BUILDDIR/conf/local.conf
+## adjustments to local.conf
 
-    ## kernel
-#    echo 'PREFERRED_PROVIDER_virtual/kernel = "linux-altera"' >> $BUILDDIR/conf/local.conf
-    echo 'PREFERRED_PROVIDER_virtual/kernel = "linux-altera-ltsi"' >> $BUILDDIR/conf/local.conf
-    echo 'PREFERRED_VERSION_linux-altera = "4.17%"' >> $BUILDDIR/conf/local.conf
+## MACHINE
+sed -i '/MACHINE ??= "qemux86"/s/.*/MACHINE ?= "cyclone5"/g' $BUILDDIR/conf/local.conf
 
-    ## toolchain
-    ## alternatively (former old toolchain): ANGSTROM_GCC_VERSION_arm = "linaro-4.9%"
-    echo 'GCCVERSION = "linaro-5.2"' >> $BUILDDIR/conf/local.conf
-    echo 'SDKGCCVERSION = "linaro-5.2"' >> $BUILDDIR/conf/local.conf
-    echo 'DEFAULTTUNE = "cortexa9hf-neon"' >> $BUILDDIR/conf/local.conf
+## deb packages
+sed -i '/PACKAGE_CLASSES ?= "package_rpm"/s/.*/PACKAGE_CLASSES ?= "package_deb"/g' $BUILDDIR/conf/local.conf
 
-    echo 'EXTRA_USERS_PARAMS = "usermod -P root root; "' >> $BUILDDIR/conf/local.conf
+## kernel
+# echo 'PREFERRED_PROVIDER_virtual/kernel = "linux-altera"' >> $BUILDDIR/conf/local.conf
+echo 'PREFERRED_PROVIDER_virtual/kernel = "linux-altera-ltsi"' >> $BUILDDIR/conf/local.conf
+echo 'PREFERRED_VERSION_linux-altera = "4.17%"' >> $BUILDDIR/conf/local.conf
 
-    ## uboot setup
-    echo 'UBOOT_CONFIG = "cyclone5-socrates"' >> $BUILDDIR/conf/local.conf
-    echo 'KERNEL_DEVICETREE = "socfpga_cyclone5_socrates.dtb"' >> $BUILDDIR/conf/local.conf
-    echo 'UBOOT_EXTLINUX_FDT_default ?= "../socfpga_cyclone5_socrates.dtb"' >> $BUILDDIR/conf/local.conf
+## toolchain
+## alternatively (former old toolchain): ANGSTROM_GCC_VERSION_arm = "linaro-4.9%"
+echo 'GCCVERSION = "linaro-5.2"' >> $BUILDDIR/conf/local.conf
+echo 'SDKGCCVERSION = "linaro-5.2"' >> $BUILDDIR/conf/local.conf
+echo 'DEFAULTTUNE = "cortexa9hf-neon"' >> $BUILDDIR/conf/local.conf
 
-fi
+echo 'EXTRA_USERS_PARAMS = "usermod -P root root; "' >> $BUILDDIR/conf/local.conf
+
+## uboot setup
+echo 'UBOOT_CONFIG = "cyclone5-socrates"' >> $BUILDDIR/conf/local.conf
+echo 'KERNEL_DEVICETREE = "socfpga_cyclone5_socrates.dtb"' >> $BUILDDIR/conf/local.conf
+echo 'UBOOT_EXTLINUX_FDT_default ?= "../socfpga_cyclone5_socrates.dtb"' >> $BUILDDIR/conf/local.conf
+
 
 ## source again, before start building
 cd ~/poky
