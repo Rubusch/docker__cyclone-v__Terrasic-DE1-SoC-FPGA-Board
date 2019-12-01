a694cc22367eafb94848b33c3b47010d6d2de595
Lothar Rubusch <l.rubusch@gmx.ch>
2019-12-01 15:59:37 +0100

    build.sh: fix injection of config files

--- docker__yocto/build.sh
+++ docker__yocto/build.sh
@@ -4,13 +4,16 @@
 export BUILDDIR=~/poky/build
 chown $(whoami):$(whoami) -R $BUILDDIR
 
+## source again, before start building
+cd ~/poky
+source oe-init-build-env $BUILDDIR
+
 ## config files
 cp -arf /home/$(whoami)/poky/meta-lothars-configs/conf/bblayers.conf.sample ${BUILDDIR}/conf/bblayers.conf
 cp -arf /home/$(whoami)/poky/meta-lothars-configs/conf/local.conf.sample ${BUILDDIR}/conf/local.conf
 
-## source again, before start building
-cd ~/poky
-source oe-init-build-env $BUILDDIR
+## adjust config files
+sed "s/  ~\/poky\//  \/home\/$(whoami)\/poky\//g" -i ${BUILDDIR}/conf/bblayers.conf
 
 ## build
 bitbake core-image-minimal
