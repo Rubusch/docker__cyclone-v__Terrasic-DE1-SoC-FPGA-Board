d8d7118dada9a1dce4337adb64d55373cdfb664e
Lothar Rubusch <l.rubusch@gmx.ch>
2020-12-01 13:18:46 +0100

    buildroot: update build.sh, fix permissions

--- docker__buildroot/build.sh
+++ docker__buildroot/build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash -e
 export DEFCONFIG=lothars__socrates_cyclone5_defconfig
-cd ~/buildroot
+export MY_HOME="$(pwd)"
+export USER="$(whoami)"
+export BUILDDIR="${MY_HOME}/buildroot"
+
+sudo chown ${USER}:${USER} -R ${BUILDDIR}
+
+cd ${MY_HOME}/buildroot
 make ${DEFCONFIG}
 make -j8
+
+echo "READY."
+echo
