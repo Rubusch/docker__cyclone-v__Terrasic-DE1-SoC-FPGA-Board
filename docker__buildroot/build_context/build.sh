4b8c201c9f789204142ce7d000b0240cfaf05427
Lothar Rubusch <l.rubusch@gmail.com>
2021-04-11 21:56:38 +0200

    build.sh: fix conditional changing ownership
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

--- docker__buildroot/build_context/build.sh
+++ docker__buildroot/build_context/build.sh
@@ -3,8 +3,10 @@ DEFCONFIG=lothars__socrates_cyclone5_defconfig
 MY_HOME="$(pwd)"
 USER="$(whoami)"
 BUILD_DIR="${MY_HOME}/buildroot"
+DL_DIR="${BUILD_DIR}/dl"
+OUTPUT_DIR="${BUILD_DIR}/output"
 
-for item in ${BUILD_DIR}; do
+for item in ${BUILD_DIR} ${DL_DIR} ${OUTPUT_DIR}; do
     if [ "${USER}" != "$(stat -c %U ${item} )" ]; then
         sudo chown ${USER}:${USER} -R ${item}
     fi
