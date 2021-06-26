efcfe83016c2ac33c97bc1ee4e2f152b3e0d619c
Lothar Rubusch <l.rubusch@gmail.com>
2021-06-26 14:44:59 +0200

    build_context: source environment after setting up bblayers

--- docker__yocto/build_context/build.sh
+++ docker__yocto/build_context/build.sh
@@ -53,19 +53,15 @@ if [[ ! -L "${YOCTO_DIR}/meta-lothars-configs" ]]; then
     sed "s/~/\/home\/$USER/g" -i ${YOCTO_DIR}/meta-lothars-configs/conf/bblayers.conf.sample
 fi
 
-## build_dir
-mkdir -p ${BUILD_DIR}
-
-## config files
-cp -arf ${YOCTO_DIR}/meta-lothars-configs/conf/bblayers.conf.sample ${BUILD_DIR}/conf/bblayers.conf
-cp -arf ${YOCTO_DIR}/meta-lothars-configs/conf/local.conf.sample ${BUILD_DIR}/conf/local.conf
-
-## adjust config files
-sed "s/  ~\/poky\//  \/home\/$(whoami)\/poky\//g" -i ${BUILD_DIR}/conf/bblayers.conf
-
 ## source, to provide environment
 cd ${YOCTO_DIR}
 source oe-init-build-env $BUILD_DIR
+
+## config files
+mkdir -p ${BUILD_DIR}/conf
+cp -arf ${YOCTO_DIR}/meta-lothars-configs/conf/bblayers.conf.sample ${BUILD_DIR}/conf/bblayers.conf
+cp -arf ${YOCTO_DIR}/meta-lothars-configs/conf/local.conf.sample ${BUILD_DIR}/conf/local.conf
+sed "s/  \/home\/user\/poky\//  \/home\/$(whoami)\/poky\//g" -i ${BUILD_DIR}/conf/bblayers.conf
 
 ## build
 cd ${BUILD_DIR}
